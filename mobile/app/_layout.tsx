e4ac1b0d91cd44467180068664046c9b2e4094f7
madarorbit <orbit.ops.digital@gmail.com>
2026-08-04 03:53:09 +0300

    fix(mobile-v2): remove unused Reanimated runtime import

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,4 @@
 import 'react-native-gesture-handler';
-import 'react-native-reanimated';
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
