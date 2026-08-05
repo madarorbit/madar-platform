c0c5d5f5bde6c51ad407879c555a5d60060321ef
madarorbit <orbit.ops.digital@gmail.com>
2026-08-06 00:03:09 +0300

    Export OpenFGA, Svix, and Nango adapters

--- src/lib/platform-integrations/index.ts
+++ src/lib/platform-integrations/index.ts
@@ -2,3 +2,6 @@ export * from './config';
 export * from './trigger';
 export * from './langfuse';
 export * from './openmeter';
+export * from './openfga';
+export * from './svix';
+export * from './nango';
